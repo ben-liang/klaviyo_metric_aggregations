f0682989cecee18ab94db0042d25af4375e55304
Ben Liang <ben.liang@klaviyo.com>
2020-01-02 15:02:47 -0500

    Added flow message IDs to metric export timeline approach & created CLI for export.

---
+++ bin/message_aggregates_from_export.py
@@ -0,0 +1,36 @@
+import json
+import argparse
+from metric_aggregates import from_individual_message_export
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Aggregate data for a one Klaviyo metric (for all messages) in an account '
+                    'using the metric timeline endpoint.')
+    parser.add_argument('api_key', type=str,
+                        help='API key (or private key) used to access the Klaviyo API for a given account.')
+    parser.add_argument('--metric_name',
+                        choices=[
+                            "Received Email",
+                            "Opened Email",
+                            "Bounced Email",
+                            "Clicked Email"
+                        ],
+                        help='Name of the message metric you want to aggregate.')
+    parser.add_argument('--start_date', type=str,
+                        help='Start date of metric export range, format `YYYY-mm-dd`')
+    parser.add_argument('--end_date', type=str,
+                        help='End date of metric export range, format `YYYY-mm-dd`')
+
+    args = parser.parse_args()
+    aggregator = from_individual_message_export.AggregatesFromIndividualMessageExport(args.api_key)
+
+    # get raw event data by hitting the timeline API & paginating through all results
+    # this step could take a while
+    metric_data = aggregator.main(args.metric_name, args.start_date, args.end_date)
+
+    # Store all data in CSV
+    filename = "../data/{api_key}_{metric_name}_aggregate_data.json".format(api_key=args.api_key,
+                                                                            metric_name=args.metric_name)
+    with (filename, "w") as file:
+        json.dump(metric_data, file, indent=4, sort_keys=True)
--- metric_aggregates/from_individual_message_export.py
+++ metric_aggregates/from_individual_message_export.py
@@ -1,5 +1,6 @@
 import requests
 import klaviyo
+from tqdm import tqdm
 from . import utils
 
 
@@ -29,22 +30,79 @@ class AggregatesFromIndividualMessageExport(object):
         more = True
         while more:
             response = requests.get(self.campaign_url(page=page)).json()
-            camps = response["data"]
+            camps = map(lambda campaign: campaign["id"], response["data"])
             campaigns.extend(camps)
             more = response["end"] < (response["total"] - 1)
             if more:
                 page += 1
         return campaigns
 
-    def get_metric_data_from_export_for_campaigns(self, metric_name, campaigns, start_date, end_date):
+    @classmethod
+    def flows_url(cls):
+        return "{base_url}/flows".format(base_url=cls.URL_BASE)
+
+    @classmethod
+    def flow_actions_url(cls, flow_id):
+        return '{base_url}/flow/{flow_id}/actions'.format(base_url=cls.URL_BASE,
+                                                          flow_id=flow_id)
+
+    @classmethod
+    def flow_action_email_url(cls, flow_id, email_id):
+        return '{base_url}/flow/{flow_id}/action/{email_id}/email'.format(base_url=cls.URL_BASE,
+                                                                          flow_id=flow_id,
+                                                                          email_id=email_id)
+
+    def get_flow_message_ids(self):
+        """
+        Gets all flow message IDs from flow API endpoints.
+
+        Executes three calls. First gets all flows, then gets all actions within each flow and looks for
+        all "SEND_MESSAGE" actions. Finally, gets all emails for "SEND_MESSAGE" actions in flows and returns
+        list of message IDs.
+
+        :return:
+        """
+        payload = {'api_key': self.api_key}
+
+        # first get all flows
+        r = requests.get(self.flows_url(), params=payload)
+        data = r.json()
+        all_flows = []
+        for flow in data['data']:
+            all_flows.append(flow['id'])
+
+        # Get all SEND_MESSAGE actions for each flow
+        flow_to_action_mapping = {}
+        ACTION_CONSTANT = "SEND_MESSAGE"
+        for each_flow in all_flows:
+            unique_flow_url = self.flow_actions_url(each_flow)
+            fr = requests.get(unique_flow_url, params=payload)
+            flow_actions = fr.json()
+            flow_to_action_mapping[each_flow] = []
+            for action in flow_actions:
+                if action['type'] == ACTION_CONSTANT:
+                    flow_to_action_mapping[each_flow].append(action['id'])
+
+        # Get all message ids for each flow SEND_MESSAGE action
+        all_message_ids = []
+        for flows in flow_to_action_mapping:
+            for email in flow_to_action_mapping[flows]:
+                unique_email_url = self.flow_action_email_url(flows, email)
+                far = requests.get(unique_email_url, params=payload)
+                all_emails_returned = far.json()
+                all_message_ids.append(all_emails_returned['id'])
+
+        return all_message_ids
+
+    def get_metric_data_from_export_for_message_ids(self, metric_name, message_ids, start_date, end_date):
         """
         Gets metric data from metric export endpoint for metric_name, campaigns list. Performs a \
         single request per campaign in campaigns list.
         """
         metric_id = utils.get_metric_id(metric_name, self.client)
         metric_data = []
-        for campaign in campaigns:
-            message = '[["$message","=","{}"]]'.format(campaign["id"])
+        for message_id in tqdm(message_ids):
+            message = '[["$message","=","{}"]]'.format(message_id)
             response = self.client.metric_export(metric_id,
                                                  start_date=start_date,
                                                  end_date=end_date,
@@ -66,9 +124,18 @@ class AggregatesFromIndividualMessageExport(object):
 
     def main(self, metric_name, start_date, end_date):
         """
-        Gets metric export data for specific metric name by first getting all campaigns,
-        then looping over campaigns list and passing each campaign ID as message ID to
+        Gets metric export data for specific metric name by first getting all message IDs
+        for campaigns & flows, then looping over message_id list and passing each message ID
         single metric export API requests.
         """
-        campaigns = self.get_campaigns()
-        return self.get_metric_data_from_export_for_campaigns(metric_name, campaigns, start_date, end_date)
+        message_ids = self.get_campaigns()
+        print("{} Campaign message IDs found for account.".format(len(message_ids)))
+        flow_ids = self.get_flow_message_ids()
+        print("{} Flow message IDs found for account.".format(len(flow_ids)))
+        message_ids.extend(flow_ids)
+        print("Getting metric export data for {metric_name} from {start_date} to {end_date} "
+              "for {message_count} messages (this may take a little while)...".format(metric_name=metric_name,
+                                                                                      message_count=len(message_ids),
+                                                                                      start_date=start_date,
+                                                                                      end_date=end_date))
+        return self.get_metric_data_from_export_for_message_ids(metric_name, message_ids, start_date, end_date)
